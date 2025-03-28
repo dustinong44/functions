298a50800f536b7479933580086d6e77a20ea328
dustinong44 <dustinong44@gmail.com>
2025-03-28 23:11:11 +0800

    working function

---
+++ lstrip_function/lstrip_function/lstrip_function.py
@@ -0,0 +1,16 @@
+def custom_lstrip(string):
+    index = 0
+    
+    # Loop and then looks for first na hindi space character
+    while index < len(string) and string[index] == " ":
+        index += 1
+    
+    
+    return string[index:]
+
+
+input_string = input("Please enter your phrase:")
+output_string = custom_lstrip(input_string)
+print(f"Input: '{input_string}'")
+print(f"Output: '{output_string}'")
+
