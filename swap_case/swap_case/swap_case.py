a968a66b83677dd6bb1406075388dd8bb782c2d5
dustinong44 <dustinong44@gmail.com>
2025-03-28 22:47:12 +0800

    first commit

--- swap_case/swap_case/swap_case.py
+++ swap_case/swap_case/swap_case.py
@@ -1,1 +1,18 @@
+def copy_isupper(s):
+    # check if meron alphabet character
+    has_alpha = False
 
+    for char in s:
+        if char.isalpha():
+            has_alpha = True
+            if not ('A' <= char <= 'Z'): #naka set sa letters for uppercase
+                return False
+
+    return has_alpha
+
+
+string_input = input ("Please Enter your string: ")
+
+string_input = string_input.copy_isupper(s)
+
+print (string_input)
