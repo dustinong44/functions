bf553f9d7d2da2d03b43bf3649ae3bc9ca637ce1
dustinong44 <dustinong44@gmail.com>
2025-03-28 22:51:16 +0800

    working code

--- swap_case/swap_case/swap_case.py
+++ swap_case/swap_case/swap_case.py
@@ -1,18 +1,20 @@
-def copy_isupper(s):
-    # check if meron alphabet character
+def is_upper_custom(s):
+    #define function that checks if all of the chracters are in uppercase
     has_alpha = False
 
     for char in s:
-        if char.isalpha():
+        if 'a' <= char <= 'z':  
+            return False
+        if 'A' <= char <= 'Z':  # both condition checks for the casing of the character
             has_alpha = True
-            if not ('A' <= char <= 'Z'): #naka set sa letters for uppercase
-                return False
 
     return has_alpha
 
 
-string_input = input ("Please Enter your string: ")
+string_input = input("Please enter your string: ")
 
-string_input = string_input.copy_isupper(s)
+is_upper = is_upper_custom(string_input)
 
-print (string_input)
+
+print(is_upper)
+
